5620e068cb5b0c1c58c1bd815f1aed198825c766
Nick Moore <nick@zoic.org>
2009-01-21 12:38:16 +0000

    bit bashing

--- radio/serial/sendrecv.c
+++ radio/serial/sendrecv.c
@@ -1,4 +1,4 @@
-/* $Id: sendrecv.c,v 1.6 2009-01-21 07:22:50 nick Exp $ */
+/* $Id: sendrecv.c,v 1.7 2009-01-21 12:38:16 nick Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,9 +34,67 @@ int baud_table[][2] = {
     { 0, 0 },
 };
 
-#define GETBITS(x,a,b,c) ((((x) >> (a)) & ((1 << ((b)+1))-1)) << (c))
+#define BITMASK(b) ((1 << (b)) - 1)
+#define GETBITS(x,a,b) (((x) >> (a)) & BITMASK(b))
 
 char preamble[] = "UUUUUUUUUU\xFF\xFF\x00";
+
+#include "symbols.h"
+#define PREAMBLE SYMBOL_RESERVED_0
+#deinfe ENDFRAME SYMBOL_RESERVED_1
+
+int bytes_to_symbols(unsigned char *bytes, int nbytes, unsigned char *symbols) {
+    symbols[0] = symbols_encode[                           GETBITS(bytes[0], 0, 7)      ];
+    if (nbytes < 2) return 1;
+    symbols[1] = symbols_encode[ GETBITS(bytes[0], 7, 1) + GETBITS(bytes[1], 0, 6) << 1 ];
+    if (nbytes < 3) return 2;
+    symbols[2] = symbols_encode[ GETBITS(bytes[1], 6, 2) + GETBITS(bytes[2], 0, 5) << 2 ];
+    if (nbytes < 4) return 3;
+    symbols[3] = symbols_encode[ GETBITS(bytes[2], 5, 3) + GETBITS(bytes[3], 0, 4) << 3 ];
+    if (nbytes < 5) return 4;
+    symbols[4] = symbols_encode[ GETBITS(bytes[3], 4, 4) + GETBITS(bytes[4], 0, 3) << 4 ];
+    if (nbytes < 6) return 5;
+    symbols[5] = symbols_encode[ GETBITS(bytes[4], 3, 5) + GETBITS(bytes[5], 0, 2) << 5 ];
+    if (nbytes < 7) return 6;
+    symbols[6] = symbols_encode[ GETBITS(bytes[5], 2, 6) + GETBITS(bytes[6], 0, 1) << 6 ];
+    if (nbytes < 8) return 7;
+    symbols[7] = symbols_encode[ GETBITS(bytes[6], 1, 7)                                ];
+    return 8;
+}
+
+int symbols_to_bytes(unsigned char *symbols, unsigned char *bytes) {
+    
+    unsigned char s0 = symbols_decode[symbols[0]];
+    unsigned char s1 = symbols_decode[symbols[1]];
+    if (s0 == -1 || s1 == -1) return 0;
+    bytes[0] = GETBITS(s0, 0, 7) + GETBITS(s1, 0, 1);
+    
+    unsigned char s2 = symbols_decode[symbols[2]];
+    if (s2 == -1) return 1;
+    bytes[1] = GETBITS(s1, 1, 6) + GETBITS(s2, 0, 2);
+    
+    unsigned char s3 = symbols_decode[symbols[3]];
+    if (s3 == -1) return 2;
+    bytes[2] = GETBITS(s2, 2, 5) + GETBITS(s3, 0, 3);
+    
+    unsigned char s4 = symbols_decode[symbols[4]];
+    if (s4 == -1) return 3;
+    bytes[3] = GETBITS(s3, 3, 4) + GETBITS(s4, 0, 4);
+    
+    unsigned char s5 = symbols_decode[symbols[5]];
+    if (s5 == -1) return 4;
+    bytes[4] = GETBITS(s4, 4, 3) + GETBITS(s5, 0, 5);
+    
+    unsigned char s6 = symbols_decode[symbols[6]];
+    if (s6 == -1) return 5;
+    bytes[5] = GETBITS(s5, 5, 2) + GETBITS(s6, 0, 6);
+    
+    unsigned char s7 = symbols_decode[symbols[7]];
+    if (s7 == -1) return 6;
+    bytes[6] = GETBITS(s6, 6, 1) + GETBITS(s7, 0, 7);
+
+    return 7;
+}
 
 int initialize_port(int fd, int baud_rate) {
 
