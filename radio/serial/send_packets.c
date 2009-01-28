8ece921fb38fc6cd83e1d6f8bd4d58ec71f997de
Nick Moore <nick@zoic.org>
2009-01-28 05:30:34 +0000

    That's everything

--- radio/serial/send_packets.c
+++ radio/serial/send_packets.c
@@ -1,4 +1,4 @@
-/* $Id: send_packets.c,v 1.5 2009-01-28 04:56:53 nick Exp $ */
+/* $Id: send_packets.c,v 1.6 2009-01-28 05:30:34 nick Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,12 +14,13 @@
 int main(int argc, char **argv) {
 
     if (argc<2) {
-        fprintf(stderr, "usage: %s <devname>\n", argv[0]);
+        fprintf(stderr, "usage: %s <devname> [<baud rate> [<num_packets>]]\n", argv[0]);
         exit(1);
     }
 
     int serial_fd = open(argv[1], O_WRONLY | O_SYNC);
-    int baud_rate = (argc>2)?atoi(argv[2]):2400;
+    int baud_rate = (argc>2)?atoi(argv[2]):9600;
+    int num_packets = (argc>3)?atoi(argv[3]):baud_rate / 200;
 
     if (serial_fd < 0) {
         fprintf(stderr, "couldn't open %s: %s", argv[1], strerror(errno));
@@ -32,8 +33,8 @@ int main(int argc, char **argv) {
         exit(3);
     }
     int i;
-    unsigned char packet_data[NUM_PACKETS][PACKET_LENGTH];
-    for (i=0; i<NUM_PACKETS; i++) {
+    unsigned char packet_data[num_packets][PACKET_LENGTH];
+    for (i=0; i<num_packets; i++) {
         if (!fread(packet_data[i], PACKET_LENGTH, 1, data_file)) {
             fprintf(stderr, "couldn't read %s: %s", DATA_FILENAME, strerror(errno));
             exit(4);
@@ -43,8 +44,8 @@ int main(int argc, char **argv) {
     int j;
     
     initialize_port(serial_fd, baud_rate);
-    for (j=0; j<NUM_PACKETS; j++) {
-        printf ("Packet %d\n", j);
+    for (j=0; j<num_packets; j++) {
+        printf ("Sending packet %d / %d\n", j, num_packets);
         send_packet(serial_fd, packet_data[j], PACKET_LENGTH);
         sleep(1);
     }
