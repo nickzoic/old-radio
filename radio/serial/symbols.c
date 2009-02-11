dc4822e468a00b5e16e9e29c2bf6d1033d1f117a
Nick Moore <nick@zoic.org>
2009-02-11 22:30:20 +0000

    oops, missed one

--- radio/serial/symbols.c
+++ radio/serial/symbols.c
@@ -1,4 +1,4 @@
-/* $Id: symbols.c,v 1.9 2009-02-11 22:29:32 nick Exp $ */
+/* $Id: symbols.c,v 1.10 2009-02-11 22:30:20 nick Exp $ */
 
 #include <stdio.h>
 
@@ -60,37 +60,37 @@ int _s2b(unsigned char *symbols, int nsym, unsigned char *bytes) {
     if (nsym < 2) return 0; 
     int s0 = symbol_decode[symbols[0]];
     int s1 = symbol_decode[symbols[1]];
-    if (s0 == -1 || s1 == -1) return 0;
+    if (s0 < 0 || s1 < 0) return 0;
     bytes[0] = GETBITS(s0, 0, 7) + ( GETBITS(s1, 0, 1) << 7);
    
     if (nsym < 3) return 1; 
     int s2 = symbol_decode[symbols[2]];
-    if (s2 == -1) return 1;
+    if (s2 < 0) return 1;
     bytes[1] = GETBITS(s1, 1, 6) + ( GETBITS(s2, 0, 2) << 6);
     
     if (nsym < 4) return 2; 
     int s3 = symbol_decode[symbols[3]];
-    if (s3 == -1) return 2;
+    if (s3 < 0) return 2;
     bytes[2] = GETBITS(s2, 2, 5) + ( GETBITS(s3, 0, 3) << 5);
     
     if (nsym < 5) return 3; 
     int s4 = symbol_decode[symbols[4]];
-    if (s4 == -1) return 3;
+    if (s4 < 0) return 3;
     bytes[3] = GETBITS(s3, 3, 4) + ( GETBITS(s4, 0, 4) << 4);
     
     if (nsym < 6) return 4; 
     int s5 = symbol_decode[symbols[5]];
-    if (s5 == -1) return 4;
+    if (s5 < 0) return 4;
     bytes[4] = GETBITS(s4, 4, 3) + ( GETBITS(s5, 0, 5) << 3);
     
     if (nsym < 7) return 5; 
     int s6 = symbol_decode[symbols[6]];
-    if (s6 == -1) return 5;
+    if (s6 < 0) return 5;
     bytes[5] = GETBITS(s5, 5, 2) + ( GETBITS(s6, 0, 6) << 2);
     
     if (nsym < 8) return 6; 
     int s7 = symbol_decode[symbols[7]];
-    if (s7 == -1) return 6;
+    if (s7 < 0) return 6;
     bytes[6] = GETBITS(s6, 6, 1) + ( GETBITS(s7, 0, 7) << 1);
 
     return 7;
