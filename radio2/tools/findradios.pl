ed20c6162df293c79fe3c64ca3afcbe5ebb72deb
Nick Moore <nick@zoic.org>
2009-11-18 06:08:27 +0000

    distkeys finds linux boxes and distributes keys findradios finds the radios on those linux boxes

---
+++ radio2/tools/findradios.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use strict;
+
+foreach my $host (<>) {
+	chomp $host;
+	my @radios = `ssh $host ls /dev/radio* 2>/dev/null`;
+	next unless @radios;
+
+	`./findradios.expect $host`;
+	foreach my $r (@radios) {
+	    chomp($r);
+	    print "$host $r\n";
+	}
+}
