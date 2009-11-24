91142cc51a03e4c267b639a57fa0b0adb7ec46a2
Nick Moore <nick@zoic.org>
2009-11-24 04:05:39 +0000

    new runmany

--- radio2/tools/runmany.pl
+++ radio2/tools/runmany.pl
@@ -1,23 +1,17 @@
 #!/usr/bin/perl -w
-# $Id: runmany.pl,v 1.1 2009-11-24 03:20:33 nick Exp $
+# $Id: runmany.pl,v 1.2 2009-11-24 04:05:39 nick Exp $
 
 use strict;
 use File::Temp qw'tempfile tempdir';
+use List::Util qw'min';
 
 my $program = shift;
 my $args = join ' ', @ARGV;
 
-my @remotes=qw'cthulhu 10.107.2.104';
+my @radios = map { [ /(\w+)\s+(/dev/radio(\d+))/ ] } <>;
+my $min_id = min( map { $_->[2] } @radios );
 
-my @radios = split "\n", `ls -1 /dev/radio*`;
-
-for my $remote (@remotes) {
-    push @radios, map {
-	"$remote:$_"
-    } split "\n", `ssh $remote 'ls -1 /dev/radio*'`;
-}
-
-printf STDERR "FOUND %d RADIOS!\n", scalar @radios;
+printf STDERR "RUNNING %d RADIOS!\n", scalar @radios;
 
 my $tempdir = tempdir( CLEANUP => 1 );
 
@@ -26,13 +20,10 @@ for my $radio (@radios) {
     if (my $pid = fork()) {
 	$waitpid{$pid} = 1;
     } else {
-        if ($radio =~ m{([^:]+):(/dev/radio(\d+))}) {
-           print STDERR "ssh $1 $program $2 $args $3 > $tempdir/$$.log\n";
-           exec("ssh $1 $program $2 $args $3 > $tempdir/$$.log");
-        } elsif ($radio =~ m{/dev/radio(\d+)} ) {
-	   print STDERR "$program $radio $args $1 > $tempdir/$$.log\n";
-	   exec("$program $radio $args $1 > $tempdir/$$.log");
-        }
+	my ($host, $device, $id) = @$radio;
+	$id = 0 if $id == $min_id;
+        print STDERR "ssh $host $program $device $id $args > $tempdir/$$.log\n";
+        exec("ssh $host $program $device $id $args > $tempdir/$$.log");
     }
 }
 
